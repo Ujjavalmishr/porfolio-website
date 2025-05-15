4aad729eea4ea79c0025f3430bb0ced99d754ad8
Ujjavalmishr <146470613+Ujjavalmishr@users.noreply.github.com>
2025-05-15 22:54:16 +0530

    Updated services section responsiveness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../../assets/footer_logo.svg'
+import logo from '../../assets/logo.png'
 import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className='footer'>
         <div className="footer-top">
             <div className="footer-top-left">
-                <img src={footer_logo} alt="" />
+                <img src={logo} alt="" className='navbar-logo' />
                 <p>I'm a frontend developer from, India. I'm fresher and experience to work in team to build projects.</p>
             </div>
             <div className="footer-top-right">
